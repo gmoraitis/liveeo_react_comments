e547c2b36915409bda27308c79434e6806fcf62c
George Moraitis <moraitis.georgios@icloud.com>
2022-04-11 13:36:11 +0300

    Added navbar and css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,20 @@
+import '../App.css'
+
 function Navbar() {
     return (
         <div>
             <header>
-                hello world!
+                <div id="brand"><a href="/">LiveEO</a></div>
+                <nav>
+                    <ul>
+                        <li><a href="/">Map</a></li>
+                        <li><a href="/">Tasks</a></li>
+                        <li><a href="/">Dashboard</a></li>
+                        <li id="comments"><a href="/">Comments</a></li>
+                        <li id="admin"><a href="/">Admin</a></li>
+                        <li id="login"><a href="/">L</a></li>
+                    </ul>
+                </nav>
             </header>
         </div>
     )
